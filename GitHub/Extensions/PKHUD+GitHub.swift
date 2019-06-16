d44218b416b70fae3b12b9671a737e323447dcd9
築山朋紀 <tomoki_sun@tomoki-sun.lan>
2019-06-17 04:31:53 +0900

    Update PKHUD

--- GitHub/Extensions/PKHUD+GitHub.swift
+++ GitHub/Extensions/PKHUD+GitHub.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2019 築山朋紀. All rights reserved.
 //
 
-import Foundation
+import PKHUD
+import RxSwift
+import RxCocoa
+
+extension HUD {
+    public class func showProgress() {
+        HUD.show(.progress, onView: UIApplication.shared.windows.last)
+    }
+    
+    public class func flashMessage(_ content: HUDContentType) {
+        HUD.flash(
+            content,
+            onView: UIApplication.shared.keyWindow,
+            delay: 2, completion: nil
+        )
+    }
+}
+
+extension SharedSequence where S == DriverSharingStrategy, E == Bool {
+    func loader() -> Disposable {
+        return self.drive(onNext: { result in
+            if result {
+                HUD.show(.progress, onView: UIApplication.shared.windows.last)
+            } else {
+                HUD.hide()
+            }
+        })
+    }
+}
