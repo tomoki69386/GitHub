6fae189d36a396ee50286c11d360f6680b0049a7
築山朋紀 <tomoki_sun@tomoki-sun.lan>
2019-06-17 05:26:48 +0900

    Update Profile

---
+++ GitHub/Common/IconViiewew.swift
@@ -0,0 +1,40 @@
+//
+//  ImageView.swift
+//  GitHub
+//
+//  Created by 築山朋紀 on 2019/06/17.
+//  Copyright © 2019 築山朋紀. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class ImageView: UIImageView {
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: self.layer.borderColor!)
+        }
+        set {
+            self.layer.borderColor = newValue?.cgColor
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return self.layer.borderWidth
+        }
+        set {
+            self.layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+}
