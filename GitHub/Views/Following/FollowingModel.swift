de77c51c42f44b1c2c2499fe5dca0e0a1ddd9122
築山朋紀 <tomoki_sun@tomoki-sun.local>
2019-06-23 21:09:08 +0900

    Add get following users

---
+++ GitHub/Views/Following/FollowingModel.swift
@@ -0,0 +1,21 @@
+//
+//  FollowingModel.swift
+//  GitHub
+//
+//  Created by 築山朋紀 on 2019/06/23.
+//  Copyright © 2019 築山朋紀. All rights reserved.
+//
+
+import RxSwift
+import RxCocoa
+
+protocol FollowingModelProtocol {
+    func getFollowingUsers(of name: String) -> Single<[UserInformation]>
+}
+
+final class FollowingModel: FollowingModelProtocol {
+    let userInfoProvider = UserInfoProvider()
+    func getFollowingUsers(of name: String) -> Single<[UserInformation]> {
+        return userInfoProvider.getFollowingUsers(of: name)
+    }
+}
